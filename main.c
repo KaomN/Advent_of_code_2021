ddd6df2ec950eea5f5c2ade8a71f6bd9d4ce58c6
Cong Nguyen <kaom.n.92@gmail.com>
2021-12-01 19:33:36 +0200

    First commit

---
+++ main.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: conguyen <conguyen@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/01 11:27:58 by conguyen          #+#    #+#             */
+/*   Updated: 2021/12/01 19:31:08 by conguyen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft/libft.h"
+#include "get_next_line.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+int	ft_compare(char* s1, char* s2)
+{
+	if (ft_atoi(s1) < ft_atoi(s2))
+		return (1);
+	return (0);
+}
+
+int main(void)
+{
+	int		fd;
+	char	*line;
+	int		ret;
+	char	*temp;
+	int		count;
+
+	fd = open("input", O_RDONLY);
+	ret = 1;
+	count = 0;
+	temp = NULL;
+	while (ret)
+	{
+		ret = get_next_line(fd, &line);
+		if (temp != NULL)
+		{
+			count += ft_compare(temp, line);
+			free(temp);
+		}
+		
+		temp = ft_strdup(line);
+	}
+	free(line);
+	free(temp);
+	printf("%d\n", count);
+	return (0);
+}
